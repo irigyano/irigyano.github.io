8b60aed9d1bdd9648d31c8d630ac8aa99270fdde
irigyano <anhsieh10665@gmail.com>
2023-07-04 15:45:53 +0800

    chore: deploy to gh-pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: { baseURL: "/aaronhs/" },
   modules: ["nuxt-content-assets", "@nuxt/content", "@nuxtjs/google-fonts"],
   googleFonts: {
     families: {
@@ -10,7 +11,7 @@ export default defineNuxtConfig({
   },
   content: {
     navigation: {
-      fields: ["publishedAt","description"],
+      fields: ["publishedAt", "description"],
     },
     highlight: {
       theme: "monokai",
