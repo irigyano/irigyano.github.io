7f1c13a0479000422495ed9b9d3cea2204d46fbc
irigyano <anhsieh10665@gmail.com>
2023-07-05 13:24:48 +0800

    feat: meta tag for SEO

--- app.vue
+++ app.vue
@@ -4,9 +4,11 @@
   </NuxtLayout>
 </template>
 
-<script setup>
+<script lang="ts" setup>
 useHead({
   titleTemplate: (title) => `${title} - Aaron Hsieh`,
+  meta: [{ name: 'description', content: "Aaron Hsieh 的部落格，紀錄轉職 Web 前端的職涯發展及日常。" }],
+  htmlAttrs: { lang: "zh-tw" },
   bodyAttrs: { class: "bg-white dark:bg-black text-black dark:text-white" },
 });
 </script>
