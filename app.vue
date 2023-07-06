d9760c548b875a851a7fc346e672d8a98151854e
irigyano <anhsieh10665@gmail.com>
2023-07-06 14:54:19 +0800

    fix: favicon path after build

--- app.vue
+++ app.vue
@@ -8,6 +8,7 @@
 useHead({
   titleTemplate: (title) => `${title} - Aaron Hsieh`,
   meta: [{ name: 'description', content: "Aaron Hsieh 的部落格，紀錄轉職 Web 前端的職涯發展及日常。" }],
+  link: [{ rel: 'icon', type: 'image/png', href: `${useRuntimeConfig().app.baseURL}favicon.ico` }],
   htmlAttrs: { lang: "zh-tw" },
   bodyAttrs: { class: "bg-white dark:bg-black text-[#555555] dark:text-[#bbbbbb]" },
 });
