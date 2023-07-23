0b99eeac27854ff2932ef4b7c15ab5daa0742db5
irigyano <anhsieh10665@gmail.com>
2023-07-23 21:49:37 +0800

    feat: add bio section

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,13 @@
 <template>
   <ContentNavigation v-slot="{ navigation }">
     <main class="animate-slide-in flex flex-col gap-10 p-2">
+      <div class="flex items-center">
+        <a href="https://github.com/irigyano/aaronhs">
+          <picture><img class="w-10 h-10 rounded-full object-cover"
+              src="https://avatars.githubusercontent.com/u/110440243?v=4"></picture>
+        </a>
+        <p class="px-2">Aaron Hsieh Frontend Developer</p>
+      </div>
       <div v-for="link of navigation[0].children.sort(
         (blog1, blog2) => {
           if (blog1.publishedAt === 'Pinned') return -1
@@ -10,7 +17,7 @@
           )
         }
       )">
-        <NuxtLink class="text-4xl font-semibold text-red-500 dark:text-blue-500 hover:opacity-70 duration-300"
+        <NuxtLink class="text-3xl font-semibold text-red-500 dark:text-blue-500 hover:opacity-70 duration-300"
           :key="link._path" :to="link._path">
           {{ link.title }}
         </NuxtLink>
