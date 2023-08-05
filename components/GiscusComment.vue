ff8ead49280cf70f3fa9ebc1f539080f1929524a
irigyano <anhsieh10665@gmail.com>
2023-08-05 14:49:53 +0800

    style: giscus theme

--- components/GiscusComment.vue
+++ components/GiscusComment.vue
@@ -1,7 +1,7 @@
 <template>
     <Giscus host="https://giscus.app" id="comments" repo="irigyano/nuxt3-markdown-blog" repoId="R_kgDOJz5Cwg"
         category="Announcements" categoryId="DIC_kwDOJz5Cws4CXi-X" mapping="pathname" reactions-enabled="0" emitMetadata="0"
-        inputPosition="top" theme="" lang="zh-TW" async />
+        inputPosition="top" theme="preferred_color_scheme" lang="zh-TW" async />
 </template>
 
 <script setup>
