816daad704308862e67629ba743571dd976078fd
irigyano <anhsieh10665@gmail.com>
2023-08-07 17:26:57 +0800

    feat: make nuxt content prose A target blank

---
+++ components/content/ProseA.vue
@@ -0,0 +1,15 @@
+<template>
+    <NuxtLink :href="href" target="_blank">
+        <slot />
+    </NuxtLink>
+</template>
+  
+<script setup lang="ts">
+
+defineProps({
+    href: {
+        type: String,
+        default: ''
+    }
+})
+</script>
