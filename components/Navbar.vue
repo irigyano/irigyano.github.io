31444c69bf76546b7c4d8dbe022e0d3740dbf9c7
irigyano <anhsieh10665@gmail.com>
2023-08-08 10:16:48 +0800

    feat: navbar transition when scroll

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,6 @@
 <template>
-  <nav class="py-4 flex items-center">
+  <nav :class="{ '-translate-y-20': hideNavbar }"
+    class="py-4 flex items-center sticky top-0 z-10 bg-white dark:bg-black duration-300">
     <NuxtLink to="/" class="pl-2 text-4xl font-extrabold text-heading">
       Look Back</NuxtLink>
     <div class="flex-1 flex justify-end pr-2">
@@ -9,3 +10,19 @@
     </div>
   </nav>
 </template>
+<script setup>
+const prevPos = ref(0)
+const hideNavbar = ref(false)
+
+onMounted(() => {
+  document.addEventListener('scroll', () => {
+    const currPos = window.scrollY
+    if (currPos > prevPos.value) {
+      hideNavbar.value = true
+    } else {
+      hideNavbar.value = false
+    }
+    prevPos.value = currPos
+  })
+})
+</script>
