270a17924184da7b22a7035c5c27614d2e45ac27
irigyano <anhsieh10665@gmail.com>
2023-08-18 14:49:32 +0800

    fix: pinned article sorting rule

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,9 @@
     <main class="animate-slide-in flex flex-col gap-10 p-2">
       <div v-for="link of navigation[0].children.filter((blog) => blog.state !== 'Hidden').sort(
         (blog1, blog2) => {
+          // what is this hacky thing kek
           if (blog1.state === 'Pinned') return -1
+          if (blog2.state === 'Pinned') return 1
           return (
             new Date(blog2.publishedAt).getTime() -
             new Date(blog1.publishedAt).getTime()
