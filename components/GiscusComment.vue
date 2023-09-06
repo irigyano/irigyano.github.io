8d71d65bc382c3aaa447f59dfce3a12d85630cf0
irigyano <anhsieh10665@gmail.com>
2023-09-06 18:15:42 +0800

    style: layout

--- components/GiscusComment.vue
+++ components/GiscusComment.vue
@@ -1,7 +1,9 @@
 <template>
-    <Giscus host="https://giscus.app" id="comments" repo="irigyano/nuxt3-markdown-blog" repoId="R_kgDOJz5Cwg"
-        category="Announcements" categoryId="DIC_kwDOJz5Cws4CXi-X" mapping="pathname" reactions-enabled="0" emitMetadata="0"
-        inputPosition="top" theme="preferred_color_scheme" lang="zh-TW" async />
+    <div class="min-h-[288px]">
+        <Giscus host="https://giscus.app" id="comments" repo="irigyano/nuxt3-markdown-blog" repoId="R_kgDOJz5Cwg"
+            category="Announcements" categoryId="DIC_kwDOJz5Cws4CXi-X" mapping="pathname" reactions-enabled="0"
+            emitMetadata="0" inputPosition="top" theme="preferred_color_scheme" lang="zh-TW" async />
+    </div>
 </template>
 
 <script setup>
