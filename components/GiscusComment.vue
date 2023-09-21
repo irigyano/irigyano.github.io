2634395ac947dc47d721b30714f9d9f0cd4f6690
irigyano <anhsieh10665@gmail.com>
2023-09-21 15:24:31 +0800

    chore: update repo path

--- components/GiscusComment.vue
+++ components/GiscusComment.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="min-h-[288px]">
-        <Giscus host="https://giscus.app" id="comments" repo="hsiean/nuxt3-markdown-blog" repoId="R_kgDOJz5Cwg"
-            category="Announcements" categoryId="DIC_kwDOJz5Cws4CXi-X" mapping="pathname" reactions-enabled="0"
-            emitMetadata="0" inputPosition="top" theme="preferred_color_scheme" lang="zh-TW" async />
+        <Giscus host="https://giscus.app" id="comments" repo="hsiean/aaronhs" repoId="R_kgDOJz5Cwg" category="Announcements"
+            categoryId="DIC_kwDOJz5Cws4CXi-X" mapping="pathname" reactions-enabled="0" emitMetadata="0" inputPosition="top"
+            theme="preferred_color_scheme" lang="zh-TW" async />
     </div>
 </template>
 
